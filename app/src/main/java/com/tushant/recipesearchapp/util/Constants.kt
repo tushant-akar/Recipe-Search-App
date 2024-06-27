0e5ef7e5fdf8638f31c7502d42e2c2e6eba2ef32
Tushant Akar <106074556+tushant-akar@users.noreply.github.com>
2024-06-28 02:39:48 +0530

    Update Constants.kt

--- app/src/main/java/com/tushant/recipesearchapp/util/Constants.kt
+++ app/src/main/java/com/tushant/recipesearchapp/util/Constants.kt
@@ -3,6 +3,6 @@ package com.tushant.recipesearchapp.util
 class Constants {
     companion object {
         const val BASE_URL = "https://api.spoonacular.com/"
-        const val API_KEY = "c954b8cb133f4a608ea47acb7a96ba76"
+        const val API_KEY = "API_KEY"
     }
 }
